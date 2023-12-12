73c2b9a807a0ced4f1bae94ad49a5af822391e0a
Nivedha Ravikumar <102785188+NivedhaRavikumar@users.noreply.github.com>
2023-12-12 18:38:42 +0530

    Update server.js

--- demo/server.js
+++ demo/server.js
@@ -1,16 +1,32 @@
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
+
 const express = require('express');
 const app = express();
 // Simulated data from Kubernetes ConfigMap and Secret
-const usernameFromConfigMap = "nivedha";
-const emailFromConfigMap = "nivedharavikumar@example.com";
-const mobileFromSecret = "**********"; // Replace with actual data retrieved from Secret
+const usernameFromConfigMap = process.env.username;
+const emailFromConfigMap = process.env.email;
+const mobileFromSecret = process.env.mobile; // Replace with actual data retrieved from Secret
 app.get('/user-info', (req, res) => {
-    // Endpoint to serve user information to the frontend
-    res.json({
-        username: process.env.username,
-        email: process.env.email,
-        mobile: process.env.mobile
-    });
+    const userInfoHtml = `
+        <html>
+        <head>
+            <title>User Information</title>
+        </head>
+        <body>
+            <h1>User Information</h1>
+            <div>
+                <p><strong>Username:</strong> ${usernameFromConfigMap}</p>
+                <p><strong>Email:</strong> ${emailFromConfigMap}</p>
+                <p><strong>Mobile:</strong> ${mobileFromSecret}</p>
+            </div>
+        </body>
+        </html>
+    `;
+    // Sending the HTML content with the injected values to the client
+    res.send(userInfoHtml);
 });
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
